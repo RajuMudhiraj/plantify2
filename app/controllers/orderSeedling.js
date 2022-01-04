8a4f60a6201bb3c5c8808d1640454dee11cc8e30
RajuMudhiraj <rajuthecodingcreature@gmail.com>
2022-01-04 09:49:22 +0530

    Added checking functionality if the Seedling for the place is Ordered or not

--- app/controllers/orderSeedling.js
+++ app/controllers/orderSeedling.js
@@ -4,29 +4,34 @@ const AddPlace = require('../models/AddPlace')
 const SeedlingOrdersPlaced = require('../models/SeedlingOrdersPlaced')
 
 const { sequelize } = require('../config/database')
-// const FlowBar = require('../models/FlowBar');
 
+const { Op } = require('sequelize')
 
 const orderSeedling = async (req, res) => {
     // const t = sequelize.transaction();
 
     try {
+        const { seedlingId, placeId } = req.body;
+        const { userId } = req.userData;
 
-        // Adding managed transation
-        const result = await sequelize.transaction(async (t) => {
+        // Checking if any Seedling ordered before
+        const checkIfSeedlingOrdered = await AddPlace.findOne({ where: { [Op.and]: [{ isSeedlingOrdered: true }, { id: placeId }] } })
+        if (checkIfSeedlingOrdered) {
+            res.status(400).json({ message: "Seedling for this place is already Ordered." })
+        }
 
-            const { seedlingId, placeId} = req.body;
-            const { userId } = req.userData;
+        // Odering Seedling
+        else {
+            // Adding managed transation
+            const result = await sequelize.transaction(async (t) => {
 
-            const seedling = await Seedling.decrement('quantity', { by: 1, where: { id: seedlingId } }, { transaction: t })
-            const place = await AddPlace.update({ isSeedlingOrdered: true }, { where: { id: placeId } }, { transaction: t })
-            const order = await SeedlingOrdersPlaced.create({ quantity: 1, UserId: userId, SeedlingId: seedlingId },{ transaction: t })
+                const seedling = await Seedling.decrement('quantity', { by: 1, where: { id: seedlingId } }, { transaction: t })
+                const place = await AddPlace.update({ isSeedlingOrdered: true }, { where: { id: placeId } }, { transaction: t })
+                const order = await SeedlingOrdersPlaced.create({ quantity: 1, UserId: userId, SeedlingId: seedlingId }, { transaction: t })
 
-            res.status(201).json({ message: "Order placed successfully!" })
-
-
-
-        })
+                res.status(201).json({ message: "Order placed successfully!" })
+            })
+        }
     }
     catch (err) {
         res.status(501).json(err + "")
