833aa4b27bb0c37908281f4685292b2f0b2295b9
RajuMudhiraj <rajuthecodingcreature@gmail.com>
2022-03-27 13:12:14 +0530

    Added swaggerUI for post method of /addPlace api endpoint

--- app/controllers/addPlace.js
+++ app/controllers/addPlace.js
@@ -12,22 +12,35 @@ exports.addPlace = async (req, res) => {
         // Adding managed transation
         const result = await sequelize.transaction(async (t) => {
 
-            const { lat, long, name, addedPlace } = req.body;
+            const { lat, long, name } = req.body;
             const { userId } = req.userData;
+            try {
 
-            const user = await User.findOne({ where: { id: userId } }, { transaction: t });
-            const place = await user.createAddPlace({ lat, long, name }, { transaction: t });
-            // const flowBar = await place.createFlowBar({ addedPlace:true }, { transaction: t });
+                const user = await User.findOne({ where: { id: userId } }, { transaction: t });
+                const place = await user.createAddPlace({ lat, long, name }, { transaction: t });
+                // const flowBar = await place.createFlowBar({ addedPlace:true }, { transaction: t });
 
-            res.status(201).json({ message: "Place added successfully!" })
+                res.status(201).json({
+                    success: true,
+                    message: "Place added successfully!",
+                })
+            }
+            catch (err) {
+                res.status(400).json({
+                    success: false,
+                    error: err + " "
+                })
+            }
 
 
 
         })
     }
     catch (err) {
-        res.status(501).json(err + "")
-
+        res.status(400).json({
+            success: false,
+            error: err + " "
+        })
     }
 }
 
--- app/routes/addPlace.js
+++ app/routes/addPlace.js
@@ -4,6 +4,95 @@ const router = express.Router();
 const { addPlace } = require('../controllers/addPlace')
 
 
+// Swagger documentation for User Schema.
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     AddPlace:
+ *       type: object
+ *       required:
+ *         - lat
+ *         - long
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: Name of the place
+ *         lat:
+ *           type: float
+ *           description: The latitude
+ *         long:
+ *           type: float
+ *           description: The longitude
+ *         isSeedlingOrdered:
+ *           type: boolean
+ *           description: The status of seedling order
+ *         isSeedlingDelivered:
+ *           type: boolean
+ *           description: The status of seedling delivery
+ *         isPlanted:
+ *           type: boolean
+ *           description: The status of seedling planted
+ *         idDonated:
+ *           type: boolean
+ *           description: The status of place donated
+ * 
+ *         
+ *       example:
+ *         name: Test place
+ *         lat: 19.11111
+ *         long: 78.11111
+ *         isSeedlingOrdered: false
+ *         isSeedlingDelivered: false
+ *         isPlanted: false
+ *         isDonated: false
+ *                
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: AddPlace
+ *   description: The place managing apis
+ */
+
+/**
+* @swagger
+* /addPlace:
+*   post:
+*     summary: Add a place
+*     tags: [AddPlace]
+*     security:
+*       - bearerAuth: []
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           example:
+*             name: Place name
+*             lat: 19.11111
+*             long: 78.11111
+*           schema:
+*             $ref: '#/components/schemas/AddPlace'
+*     responses:
+*       201:
+*         description: The response of adding place.
+*         content:
+*           application/json:
+*             example:
+*               success: true
+*               message: Place added successfully! 
+*       400:
+*         description: The error message.
+*         content:
+*           application/json:
+*             example:
+*               success: false
+*               message: Error message.
+*/
+
+
+
 // Handling POST request to /flowBar
 router.post('/', addPlace)
 
